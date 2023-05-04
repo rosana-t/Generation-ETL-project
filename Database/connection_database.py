ce273af5a2b2d0171c5f2d2fc67dd3041d35dc1d
Rosana <rosana-t@hotmail.co.uk>
2023-05-04 18:32:40 +0100

    database redshift tables files

--- Database/connection_database.py
+++ Database/connection_database.py
@@ -4,18 +4,29 @@ from dotenv import load_dotenv
 
 load_dotenv()
 HOST = os.environ.get("postgres_host")
+PORT = os.environ.get("postfres_port")
 USER = os.environ.get("postgres_user")
 PASSWORD = os.environ.get("postgres_pass")
 WAREHOUSE_DB_NAME = os.environ.get("postgres_db")
 
-def setup_db_connection(host=HOST, user=USER, password=PASSWORD, warehouse_db_name=WAREHOUSE_DB_NAME):
+def setup_db_connection(ps_host, ps_port, ps_dbname, ps_user, ps_password):
     
     conn = psycopg2.connect(
-        host="localhost",
-        database="daily_grind",
-        user="postgres",
-        password="password"
+        host=ps_host,
+        port = ps_port,
+        database= ps_dbname,
+        user= ps_user,
+        password= ps_password
     )
     
     return conn
 
+y = setup_db_connection(
+    ps_host = HOST,
+    ps_port = PORT,
+    ps_dbname = WAREHOUSE_DB_NAME,
+    ps_user = USER,
+    ps_password = PASSWORD
+)
+
+print(y)
