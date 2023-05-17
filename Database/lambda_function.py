aebc8e8687e4f0d9bb730e70b06aa7c052c74978
Mihaly Jeles <mihaly.jeles@gmail.com>
2023-05-17 10:54:45 +0100

    lambda3

--- Database/lambda_function.py
+++ Database/lambda_function.py
@@ -30,7 +30,7 @@ from redshift_load_functions import *
     
 #     return raw_sales_data
     
-#Hello my friend!
+#Hello my friend!f
 def lambda_handler(event, context):
     print(f"lambda_handler called event ={event}")
     try:
