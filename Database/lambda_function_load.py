9960bf6e6e504deea9ada6df38d419b406366094
Mihaly Jeles <mihaly.jeles@gmail.com>
2023-05-17 17:40:38 +0100

    working on the sqs

--- Database/lambda_function_load.py
+++ Database/lambda_function_load.py
@@ -10,23 +10,32 @@ from redshift_load_functions import *
 
 def lambda_handler(event, context):
     try:
-        sqs_client = boto3.client('sqs')
-        response = sqs_client.receive_message(
-        QueueUrl="https://sqs.eu-west-1.amazonaws.com/015206308301/daily-grind-queue2",
-        MaxNumberOfMessages=1,
-        WaitTimeSeconds=10,
-    )
+    #     sqs_client = boto3.client('sqs')
+    #     response = sqs_client.receive_message(
+    #     QueueUrl="https://sqs.eu-west-1.amazonaws.com/015206308301/daily-grind-queue2",
+    #     MaxNumberOfMessages=1,
+    #     WaitTimeSeconds=10,
+    # )
 
-        print(f"Number of messages received: {len(response.get('Messages', []))}")
+    #     print(f"Number of messages received: {len(response.get('Messages', []))}")
 
-        for message in response.get("Messages", []):
-            message_body = message["Body"]
-        print(f"Message body: {json.loads(message_body)}")
-        print(f"Receipt Handle: {message['ReceiptHandle']}")
+    #     for message in response.get("Messages", []):
+    #         message_body = message["Body"]
+    #     print(f"Message body: {json.loads(message_body)}")
+    #     print(f"Receipt Handle: {message['ReceiptHandle']}")
         
 
-        print(f"lambda_handler connecting to Redshift")
-        print(f"lambda_handler connecting to Redshift; for file_key = {file_key}")
+    #     print(f"lambda_handler connecting to Redshift")
+    #     print(f"lambda_handler connecting to Redshift; for file_key = {file_key}")
+
+        x = event['Records'][0]['body']
+    #   list_of_branches = x['branch']
+    #   list_of_unique_product_dicts = x['product']
+    #   data_for_orders_and_transaction_table = x['orders_and_transaction']
+        print(x['branch'])
+        print(x['product'])
+        print(x['orders_and_transaction'])
+        print(event)
         
         ssm_client = boto3.client('ssm')
         parameter_details = ssm_client.get_parameter(Name='daily-grind-redshift-settings')
